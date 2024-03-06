f363bc0da4e9ffb5b094a3b20871d920211b6e6f
maxxizup <maksimkaxd1@gmail.com>
2024-03-07 03:42:45 +1000

    break-camel-case.js

--- 6kyu/break-camel-case.js
+++ 6kyu/break-camel-case.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// EXAMPLES:
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+// ✅ SOLUTION: 
+
 function solution(str) {
     let i;
     let nextChar;
