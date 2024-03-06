151ff0096fdef83f810340e290f2df668c338be7
maxxizup <maksimkaxd1@gmail.com>
2024-03-07 00:26:07 +1000

    some text changes

--- 7kyu/you-re-a-square.js
+++ 7kyu/you-re-a-square.js
@@ -1,4 +1,4 @@
-// ⁉️ DESCRIPTION:
+// ❓ DESCRIPTION:
 
 // A square of squares
 // You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
